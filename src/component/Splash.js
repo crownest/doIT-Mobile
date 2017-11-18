c2dc8e105eb93462143c87f19b03de82ff888d5b
Kübra Çınarlar <32637452+kucins@users.noreply.github.com>
2017-11-18 19:09:34 +0300

    refs #2: Splash screen created

---
+++ src/component/Splash.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { 
+  Image, 
+  View,
+  StyleSheet
+} from 'react-native';
+
+class Splash extends Component {
+
+  componentWillMount () {
+    setTimeout (() => {
+        this.props.navigation.navigate('Register');
+    }, 2000); 
+}
+  render() {
+    return (
+      <View style={styles.container}>
+      <Image
+        source={require('./img/bg.png')}
+        style={styles.bg}
+      />
+      <Image
+        source={require('./img/logo.png')}
+      />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  bg: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%'
+  }
+});
+  
+export default Splash;
